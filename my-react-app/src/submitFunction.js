78a49ff297880c0931d85738a3e3ef08a7a30567
MehranJamal <114913529+Mehran-Jamal@users.noreply.github.com>
2024-03-15 15:46:17 +0530

    added url

--- my-react-app/src/submitFunction.js
+++ my-react-app/src/submitFunction.js
@@ -1,9 +1,10 @@
+// submit-function.js
 const axios = require('axios');
 
 const submitFunction = async () => {
-  const FUNC_TOKEN = 'your_func_token_here';
-  const YOUR_FUNC_URL =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJwd1NwSjhQbkZaUjhEZjJROVI2QVdZVDJuN2syIiwibWljcm9zZXJ2aWNlcyI6WyJ3YWtlZmxvdy1lbWFpbHMiLCJ3YWtlZmxvdy10aW1lciIsIndha2VmbG93LTJmYSIsIndha2VmbG93LXBheW1lbnRzIiwid2FrZWZsb3ctdHdpbGlvIiwid2FrZWZsb3ctbnVtYmVyNCJdLCJpYXQiOjE3MTA0OTMxNjcsImV4cCI6MTc0MjAyOTE2N30.PJI2sov-qTa8mvtRBRgoYlfYCVDSexsMPj7oLMC9tVU';
+  const FUNC_TOKEN =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJwd1NwSjhQbkZaUjhEZjJROVI2QVdZVDJuN2syIiwibWljcm9zZXJ2aWNlcyI6WyJ3YWtlZmxvdy1lbWFpbHMiLCJ3YWtlZmxvdy10aW1lciIsIndha2VmbG93LTJmYSIsIndha2VmbG93LXBheW1lbnRzIiwid2FrZWZsb3ctdHdpbGlvIiwid2FrZWZsb3ctbnVtYmVyNCJdLCJpYXQiOjE3MTA0OTc2OTAsImV4cCI6MTc0MjAzMzY5MH0.fcu95m5x219T4j1a5P_nUYor5KQcPY6L89fgnj5dZaQ'; // Replace with your func.live token
+  const YOUR_FUNC_URL = 'https://fantastic-florentine-dfa0c7.netlify.app'; // Replace with your serverless function URL
 
   try {
     const response = await axios.post(
